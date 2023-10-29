47343a8b1dd7c922ce32231c8d6d45a431ae1603
pasan1227 <pasanratnayake@gmail.com>
2023-10-29 12:40:29 +0530

    Add navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+
 import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
