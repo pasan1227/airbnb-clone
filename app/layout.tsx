6516f86e6facc52f8a212665b9540a9cbb8793b4
pasan1227 <pasanratnayake@gmail.com>
2023-10-30 12:15:42 +0530

    Add modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <Modal title="Hello world" isOpen />
         <Navbar />
         {children}
       </body>
