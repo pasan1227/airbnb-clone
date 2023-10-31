1c4d8da2a163e489a22107a9ba48b532806b48dc
pasan1227 <pasanratnayake@gmail.com>
2023-10-31 11:33:44 +0530

    Complete UI of register modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Nunito } from "next/font/google";
 
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -20,7 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <Modal title="Hello world" isOpen />
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
